f9d6333c8f9a3d5c91bbf3caf9d7ac460208593c
PEDRO HENRIQUE DE ANDRADE LINS <pedro.hlins@a.ficr.edu.br>
2024-08-20 11:46:10 -0300

    20/08/2024 11:46

--- Pages/Home/JavaScript/index.js
+++ Pages/Home/JavaScript/index.js
@@ -7,8 +7,44 @@ let responseObject = {};
 
 if (userData) {
   responseObject = JSON.parse(userData);
-  console.log(responseObject);
+  console.log(responseObject.data);
 } else {
   console.log("problema meu nobre");
   window.location.href = "../../../index.html";
 }
+
+// Funcao para que todo o elemento que tiver "data-nomeDoValor" como classe, receberá um texto com o valor do usuario/fornecedor
+let elementsForNome = document.getElementsByClassName("data-nome")
+Object.entries(elementsForNome).forEach((e)=>{
+  e[1].innerHTML = String(responseObject.data.nome)
+})
+
+let elementsForBairro = document.getElementsByClassName("data-bairro")
+Object.entries(elementsForBairro).forEach((e)=>{
+  e[1].innerHTML = String(responseObject.data.bairro)
+})
+
+let elementsForCidade = document.getElementsByClassName("data-cidade")
+Object.entries(elementsForCidade).forEach((e)=>{
+  e[1].innerHTML = String(responseObject.data.cidade)
+})
+
+let elementsForCNPJ = document.getElementsByClassName("data-cnpj")
+Object.entries(elementsForCNPJ).forEach((e)=>{
+  e[1].innerHTML = String(responseObject.data.cnpj)
+})
+
+let elementsForEstado = document.getElementsByClassName("data-estado")
+Object.entries(elementsForEstado).forEach((e)=>{
+  e[1].innerHTML = String(responseObject.data.estado)
+})
+
+let elementsForNumero = document.getElementsByClassName("data-numero")
+Object.entries(elementsForNumero).forEach((e)=>{
+  e[1].innerHTML = String(responseObject.data.numero)
+})
+
+let elementsForTelefone = document.getElementsByClassName("data-telefone")
+Object.entries(elementsForTelefone).forEach((e)=>{
+  e[1].innerHTML = String(responseObject.data.telefone)
+})
--- Pages/SignUp/JavaScript/POST.js
+++ Pages/SignUp/JavaScript/POST.js
@@ -1,9 +1,68 @@
+
+const GetSignUp = async (obj) => {
+    event.preventDefault(); // Evita o recarregamento da página
+    try {
+        const response = await axios.post(
+            `https://e-commerce-theta-drab.vercel.app/fornecedor/sign`, // URL da API
+            {
+                nome: obj.nome,
+                telefone: obj.telefone,
+                estado: obj.estado,
+                cidade: obj.cidade,
+                bairro: obj.bairro,
+                numero: parseInt(obj.numero),
+                cnpj: obj.cnpj,
+            },
+            {
+                params: {
+                    email: obj.email,
+                    senha: obj.senha,
+                },
+            }
+        );
+        return {
+            data: response.data,
+            status: response.status
+        }
+    } catch (error) {
+        console.log(error);
+    }
+    console.log("- recebido -");
+};
+
+const GetSignIn = async (email, senha) => {
+    try {
+        event.preventDefault();
+    } catch (e) {} // Evita o recarregamento da página
+    try {
+        const response = await axios.get(
+            `https://e-commerce-theta-drab.vercel.app/fornecedor/sign`, // URL da API
+            {
+                params: {
+                    email: email,
+                    senha: senha,
+                },
+            }
+        );
+        localStorage.setItem("email", email);
+        localStorage.setItem("senha", senha);
+        localStorage.setItem("userData", JSON.stringify(response.data));
+    } catch (error) {
+        console.log("new Error");
+        console.log(error.message);
+    }
+};
+
+
 const PostFornecedor = async () => {
     const color1 = "#f1f1f1";
     const color2 = "#242424";
 
     const obj = {};
     const getValues = document.getElementsByClassName("abc");
+    const btnContinuar = document.getElementById("submit")
+    btnContinuar.style.pointerEvents = "none"
+    btnContinuar.innerHTML = `<img src="../../Media/loading-svg.png" id="loading" alt="">`
     Object.entries(getValues).forEach((e) => {
         if (e[1].type === "checkbox") {
             if (e[1].checked == true) {
@@ -18,9 +77,19 @@ const PostFornecedor = async () => {
 
     if (Object.keys(obj).length == 10) {
         // Funcao de post
-        await GetSignUp(obj);
-        // AnimationOutBack((window.location.href = "/Pages/Home/index.html"));
-        console.log("aprovado");
+        const response = await GetSignUp(obj);
+        
+        console.log("response data: ", response.data);
+        console.log(`status(${response.status})`)
+
+        if(response.status == 201){
+            console.log(obj.email, obj.senha)
+            await GetSignIn(obj.email, obj.senha)
+            AnimationOutBack((window.location.href = "/Pages/Home/index.html"))
+        }
+
+        btnContinuar.style.pointerEvents = "initial"
+        btnContinuar.innerHTML = `Continuar`
     } else {
         Swal.fire({
             title: "Algo de errado!",
@@ -33,6 +102,8 @@ const PostFornecedor = async () => {
             color: color1,
             heightAuto: false,
         });
+        btnContinuar.style.pointerEvents = "initial"
+        btnContinuar.innerHTML = `Continuar`
     }
 
     // Fim da funcao
